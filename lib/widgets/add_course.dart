94f5cdc1d270282ccb5050679d26d08ca78bcf31
unknown <mw.sabry@gmail.com>
2024-05-28 05:08:59 +0300

    first commit

--- lib/widgets/add_course.dart
+++ lib/widgets/add_course.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:power_campus/providers/added_course_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
